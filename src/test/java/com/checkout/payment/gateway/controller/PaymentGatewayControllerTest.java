3330f2aad0fb8f6fd7e4f8b76d72adc0b782dc1f
Veli Burak Celen <celenburak@gmail.com>
2025-04-14 22:21:00 +0100

    fix using the bank simulator client

--- src/test/java/com/checkout/payment/gateway/controller/PaymentGatewayControllerTest.java
+++ src/test/java/com/checkout/payment/gateway/controller/PaymentGatewayControllerTest.java
@@ -24,6 +24,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import org.springframework.test.web.servlet.ResultActions;
@@ -37,7 +38,7 @@ class PaymentGatewayControllerTest {
   private MockMvc mvc;
   @Autowired
   PaymentsRepository paymentsRepository;
-  @Mock
+  @MockBean
   private BankSimulatorClient bankSimulatorClient;
 
   @BeforeEach
